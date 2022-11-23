58907f91824ccd235d8222e631ee63be265e19eb
Usirusen Ikon <lexicon048@gmail.com>
2022-11-23 19:17:19 +0100

    Create homepage reducer and fetch api

--- src/redux/HomePage/homePage.js
+++ src/redux/HomePage/homePage.js
@@ -1,11 +1,30 @@
-import { createAsyncThunk } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 
 const FETCH_CURRENCY = 'store/HomePage/FETCH_CURRENCY';
 
 const fetchCurrency = createAsyncThunk(FETCH_CURRENCY, async () => {
-  const response = await fetch('https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies.min.json');
+  const response = await fetch(
+    'https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies.min.json',
+  );
   const currencies = await response.json();
-  return currencies;
+  const newArr = Object.keys(currencies).map((key, id) => ({ id, key: currencies[key] }));
+  return newArr.slice(1, 12);
 });
 
-export default { fetchCurrency };
+const currencySlice = createSlice({
+  name: 'currency',
+  initialState: [],
+  reducers: {
+    setCurrrency: (state, action) => ({
+      ...state,
+      currency: [...action.payload],
+    }),
+  },
+  extraReducers: {
+    [fetchCurrency.fulfilled]: (state, action) => action.payload,
+  },
+});
+
+export default currencySlice.reducer;
+export const { setCurrrency } = currencySlice.actions;
+export { fetchCurrency };
